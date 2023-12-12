61ad622f9fc7000c4a7cf55f79830cf7a9c9e0d0
jeremyrdavis <jeremyrdavis@mac.com>
2023-12-12 08:17:29 -0500

    initial

---
+++ src/main/java/io/arrogantprogrammer/GreetingResource.java
@@ -0,0 +1,23 @@
+package io.arrogantprogrammer;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from RESTEasy Reactive";
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloPost(String name) {
+        return "Hello," + name + " from RESTEasy Reactive";
+    }
+}
